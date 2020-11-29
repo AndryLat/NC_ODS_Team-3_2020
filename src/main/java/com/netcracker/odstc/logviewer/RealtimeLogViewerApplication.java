c96ad7f3c456dbc43091360cf1a50140ef6ab0d1
Andrii Latyshev <andrii.latyshev@gmail.com>
2020-11-29 22:12:02 +0200

    Init Spring boot app.

---
+++ src/main/java/com/netcracker/odstc/logviewer/RealtimeLogViewerApplication.java
@@ -0,0 +1,13 @@
+package com.netcracker.odstc.logviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeLogViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeLogViewerApplication.class, args);
+	}
+
+}
