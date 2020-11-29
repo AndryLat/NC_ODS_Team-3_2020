c12ff4dba5ea9a6fcd7db142a99114f55c049e68
Andrii Latyshev <andrii.latyshev@gmail.com>
2020-11-29 22:12:02 +0200

    Init Spring boot app.

---
+++ src/test/java/com/netcracker/odstc/logviewer/RealtimeLogViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netcracker.odstc.logviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeLogViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
