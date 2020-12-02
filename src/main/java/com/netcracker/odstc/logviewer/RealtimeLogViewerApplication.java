2e9db54fcee9f930841f44a4cd93be44185795ec
RisedWarrior <aleksandr.ogoliuk@gmail.com>
2020-12-02 12:56:27 +0200

    Exclude DataSource for building.

--- src/main/java/com/netcracker/odstc/logviewer/RealtimeLogViewerApplication.java
+++ src/main/java/com/netcracker/odstc/logviewer/RealtimeLogViewerApplication.java
@@ -2,9 +2,10 @@ package com.netcracker.odstc.logviewer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class RealtimeLogViewerApplication {
 
 	public static void main(String[] args) {
