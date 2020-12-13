9a59bb586f702f3f6ee5dce5939ee9dc2b05fe33
LeshchenkoKate <leshchenkok16@gmail.com>
2020-12-13 19:01:32 +0200

    OD20DEVAT3-39 add some methods to EAVObjectDAO

--- src/main/java/com/netcracker/odstc/logviewer/dao/EAVObjectDAO.java
+++ src/main/java/com/netcracker/odstc/logviewer/dao/EAVObjectDAO.java
@@ -10,6 +10,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -25,6 +26,61 @@ public class EAVObjectDAO {
 
     private BigInteger objectId;
 
+    public List<EAVObject> findObjectsByObjectTypeId(BigInteger objectTypeId) {
+        List<Object> objectIds = jdbcTemplate.queryForObject("SELECT object_id FROM OBJECTS WHERE OBJECT_TYPE_ID  = ?", new ObjectMapper(), objectTypeId);
+
+        if (objectIds == null) {
+            throw new EAVAttributeException("Object id cant be find in DataBase or its corrupted by object type id");
+        }
+        List<EAVObject> eavObjects = new ArrayList<>();
+        EAVObject eavObject;
+        for (int i = 0; i < objectIds.size(); i++) {
+            eavObject = new EAVObject((BigInteger) objectIds.get(i));
+            eavObjects.add(eavObject);
+        }
+        return eavObjects;
+    }
+
+    public void saveObjects(EAVObject... eavObjects) {
+        for (EAVObject eavObject : eavObjects) {
+            String updateObjectSQL = "MERGE INTO OBJECTS p USING (SELECT ? object_id, ? name,? parent_id,? object_type_id FROM DUAL) p1 ON (p.object_id = p1.object_id)WHEN MATCHED THEN UPDATE SET p.name = p1.name,p.PARENT_ID=p1.parent_id WHEN NOT MATCHED THEN INSERT (p.object_id, p.name,p.OBJECT_TYPE_ID,p.PARENT_ID)    VALUES (p1.object_id, p1.name,p1.object_type_id,p1.parent_id)";
+            jdbcTemplate.update(updateObjectSQL,
+                    objectId,
+                    eavObject.getName(),
+                    eavObject.getParentId(),
+                    eavObject.getObjectTypeId());
+            eavObject.setObjectId(objectId);
+        }
+    }
+
+    public EAVObject getObjectById(BigInteger id, BigInteger... attributeIds) {
+        EAVObject eavObject = new EAVObject(id);
+        List<Object> columns = jdbcTemplate.queryForObject("SELECT NAME, PARENT_ID, OBJECT_TYPE_ID FROM OBJECTS WHERE object_id = ?", new ObjectMapper(), id);
+        if (columns == null || columns.size() < 3) {
+            throw new EAVAttributeException("Object cant be find in DataBase or its corrupted");
+        }
+
+        eavObject.setParentId((BigInteger) columns.remove(0));
+        eavObject.setObjectTypeId((BigInteger) columns.remove(0));
+        eavObject.setName((String) columns.remove(0));
+
+
+        for (BigInteger attributeId : attributeIds) {
+            List<Map.Entry<BigInteger, Attribute>> objectAttributes = jdbcTemplate.query("SELECT ATTR_ID,VALUE,DATE_VALUE,LIST_VALUE_ID FROM ATTRIBUTES WHERE ATTR_ID = ?",
+                    new AttributeMapper(),
+                    attributeId);
+
+            HashMap<BigInteger, Attribute> attributes = new HashMap<>();
+            for (Map.Entry<BigInteger, Attribute> attribute :
+                    objectAttributes) {
+                attributes.put(attribute.getKey(), attribute.getValue());
+            }
+            eavObject.setAttributes(attributes);
+        }
+        return eavObject;
+
+    }
+
     public void saveObject(EAVObject eavObject) {
         objectId = eavObject.getObjectId();
         if (objectId == null) {
@@ -37,7 +93,7 @@ public class EAVObjectDAO {
                 eavObject.getName(),
                 eavObject.getParentId(),
                 eavObject.getObjectTypeId());
-
+        eavObject.setObjectId(objectId);
     }
 
     public void saveAttributes(BigInteger objectId, Map<BigInteger, Attribute> attributes) {
@@ -117,7 +173,12 @@ public class EAVObjectDAO {
         return eavObject;
     }
 
-    public BigInteger getObjectId(){
+    public BigInteger getObjectId() {
         return objectId;
+    }
+
+    public void deleteById(BigInteger id) {
+        String sql = "DELETE FROM OBJECTS WHERE object_id = ?";
+        jdbcTemplate.update(sql, id);
     }
 }
