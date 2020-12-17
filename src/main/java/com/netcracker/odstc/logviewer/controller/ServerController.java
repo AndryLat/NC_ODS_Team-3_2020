b8898e32ded584fc5db847143c351385f5d0675f
Valdemar <arnik.20100608@gmail.com>
2020-12-17 18:44:57 +0200

    OD20DEVAT3-41 Add serverService and OD20DEVAT3-48 serverController

---
+++ src/main/java/com/netcracker/odstc/logviewer/controller/ServerController.java
@@ -0,0 +1,65 @@
+package com.netcracker.odstc.logviewer.controller;
+
+
+import com.netcracker.odstc.logviewer.models.Server;
+import com.netcracker.odstc.logviewer.service.ServerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigInteger;
+
+@RequestMapping("/server")
+@RestController
+public class ServerController {
+
+    private final ServerService serverService;
+
+    public ServerController(ServerService serverService) {
+        this.serverService = serverService;
+    }
+
+/*
+    @GetMapping("/all")
+    public List<Server> findAll() {
+        return serverService.findAll();
+    }
+*/
+
+    @PostMapping("/add")
+    public ResponseEntity<Server> add(@RequestBody Server server) {
+        if (server == null) {
+            return new ResponseEntity("Server shouldn't be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+        serverService.save(server);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Server> update(@RequestBody Server server) {
+        if (server == null) {
+            return new ResponseEntity("Server shouldn't be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+        serverService.update(server);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping("/id/{id}")
+    public ResponseEntity<Server> findById(@PathVariable BigInteger id) {
+        if (id.equals(0) || id == null){
+           return new ResponseEntity("Id shouldn't be 0 or null",HttpStatus.NOT_ACCEPTABLE);
+        }
+        return ResponseEntity.ok(serverService.findById(id));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Server> deleteById(@PathVariable BigInteger id){
+        if (id.equals(0) || id== null){
+            return new ResponseEntity("Id shouldn't be 0 or null",HttpStatus.NOT_ACCEPTABLE);
+        }
+        serverService.deleteById(id);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+}
+
---
+++ src/main/java/com/netcracker/odstc/logviewer/service/ServerService.java
@@ -0,0 +1,67 @@
+package com.netcracker.odstc.logviewer.service;
+
+import com.netcracker.odstc.logviewer.dao.EAVObjectDAO;
+import com.netcracker.odstc.logviewer.models.Server;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import java.math.BigInteger;
+import java.util.List;
+
+
+@Component
+public class ServerService {
+
+    private final EAVObjectDAO eavObjectDAO;
+    private final Class<Server> server = Server.class;
+    private final BigInteger idType = BigInteger.valueOf(2);
+
+    public ServerService(@Qualifier("EAVObjectDAO") EAVObjectDAO eavObjectDAO) {
+        this.eavObjectDAO = eavObjectDAO;
+    }
+/*
+
+    public List<Server> findAll(){
+        return eavObjectDAO.getObjectsByObjectTypeId(idType, server);
+    }
+*/
+
+    public Server findById(BigInteger id) {
+        if (isIdValid(id)) {
+            return eavObjectDAO.getObjectById(id, server);
+        }
+        return new Server();
+    }
+
+    public void save(Server server){
+        if (isServerValid(server)){
+            eavObjectDAO.saveObject(server);
+        }
+    }
+
+    public void update(Server server) {
+        if(isServerValid(server)){
+            eavObjectDAO.saveObject(server);
+        }
+    }
+
+    public void deleteById(BigInteger id){
+        if(isIdValid(id)){
+            eavObjectDAO.deleteById(id);
+        }
+    }
+
+    private boolean isIdValid(BigInteger id){
+        return !id.equals(0) && !(id == null);
+    }
+
+    private boolean isServerValid(Server server){
+        if (server == null){return false;}
+        if (server.getIp() == null || server.getIp().trim().length() == 0){ return false;}
+        if (server.getLogin() == null|| server.getLogin().trim().length() == 0){ return false;}
+        if (server.getPassword() == null || server.getPassword().trim().length() == 0) { return false;}
+        if (server.getProtocol() == null){ return false;}
+        if (server.getPort() == 0){ return false;}
+        return true;
+    }
+}
