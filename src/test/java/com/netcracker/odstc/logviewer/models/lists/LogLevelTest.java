ff50da1e1c45d0e5def17a70aa8a82c276e371ef
RisedWarrior <aleksandr.ogoliuk@gmail.com>
2020-12-19 13:54:25 +0200

    OD20DEVAT3-18, OD20DEVAT3-45 Test changes according to model.

--- src/test/java/com/netcracker/odstc/logviewer/models/lists/LogLevelTest.java
+++ src/test/java/com/netcracker/odstc/logviewer/models/lists/LogLevelTest.java
@@ -8,22 +8,22 @@ class LogLevelTest {
 
     @Test
     void testGetValue() {
-        assertEquals(7, LogLevel.SEVERE.getValue());
-        assertEquals(8, LogLevel.WARNING.getValue());
-        assertEquals(9, LogLevel.INFO.getValue());
-        assertEquals(10, LogLevel.CONFIG.getValue());
-        assertEquals(11, LogLevel.FINE.getValue());
-        assertEquals(12, LogLevel.FINER.getValue());
-        assertEquals(13, LogLevel.FINEST.getValue());
-        assertEquals(14, LogLevel.DEBUG.getValue());
-        assertEquals(15, LogLevel.TRACE.getValue());
-        assertEquals(16, LogLevel.ERROR.getValue());
-        assertEquals(17, LogLevel.FATAL.getValue());
+        assertEquals(9, LogLevel.SEVERE.getValue());
+        assertEquals(10, LogLevel.WARNING.getValue());
+        assertEquals(11, LogLevel.INFO.getValue());
+        assertEquals(12, LogLevel.CONFIG.getValue());
+        assertEquals(13, LogLevel.FINE.getValue());
+        assertEquals(14, LogLevel.FINER.getValue());
+        assertEquals(15, LogLevel.FINEST.getValue());
+        assertEquals(16, LogLevel.DEBUG.getValue());
+        assertEquals(17, LogLevel.TRACE.getValue());
+        assertEquals(18, LogLevel.ERROR.getValue());
+        assertEquals(19, LogLevel.FATAL.getValue());
     }
 
     @Test
     void testGetByID() {
-        assertEquals(LogLevel.ERROR, LogLevel.getByID(16));
+        assertEquals(LogLevel.ERROR, LogLevel.getByID(18));
         assertThrows(IllegalArgumentException.class, () -> LogLevel.getByID(42));
     }
 
