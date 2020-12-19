449532ccf26ada11b344b4e980a4eb4c80a955b7
nick-gassiy <aezakminickita@yahoo.com>
2020-12-19 18:46:46 +0200

    directory

---
+++ src/main/java/com/netcracker/odstc/logviewer/controller/DirectoryController.java
@@ -0,0 +1,61 @@
+package com.netcracker.odstc.logviewer.controller;
+
+import com.netcracker.odstc.logviewer.models.Directory;
+import com.netcracker.odstc.logviewer.service.DirectoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigInteger;
+
+@RequestMapping("/directory")
+@RestController
+public class DirectoryController {
+    private final DirectoryService directoryService;
+    private String directoryNullMessage = "Directory shouldn't be 0 or null";
+    private String directoryIdNullMessage = "Id shouldn't be 0 or null";
+
+    public DirectoryController(DirectoryService directoryService) {
+        this.directoryService = directoryService;
+    }
+
+
+    @PostMapping("/add")
+    public ResponseEntity<Directory> add(@RequestBody Directory directory) {
+        if (directory == null) {
+            return new ResponseEntity(directoryNullMessage, HttpStatus.NOT_ACCEPTABLE);
+        }
+        directoryService.save(directory);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Directory> update(@RequestBody Directory directory) {
+        if (directory == null) {
+            return new ResponseEntity(directoryNullMessage, HttpStatus.NOT_ACCEPTABLE);
+        }
+        directoryService.save(directory);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Directory> deleteById(@PathVariable BigInteger id){
+        if (id == null || id.equals(BigInteger.valueOf(0))){
+            return new ResponseEntity(directoryNullMessage,HttpStatus.NOT_ACCEPTABLE);
+        }
+        directoryService.deleteById(id);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping("/id/{id}")
+    public ResponseEntity<Directory> findById(@PathVariable BigInteger id) {
+        if (id == null || id.equals(BigInteger.valueOf(0))){
+            return new ResponseEntity(directoryIdNullMessage,HttpStatus.NOT_ACCEPTABLE);
+        }
+        return ResponseEntity.ok(directoryService.findById(id));
+    }
+
+
+
+
+}
---
+++ src/main/java/com/netcracker/odstc/logviewer/service/DirectoryService.java
@@ -0,0 +1,48 @@
+package com.netcracker.odstc.logviewer.service;
+
+import com.netcracker.odstc.logviewer.dao.EAVObjectDAO;
+import com.netcracker.odstc.logviewer.models.Directory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import java.math.BigInteger;
+import java.util.List;
+
+@Component
+public class DirectoryService {
+    private final EAVObjectDAO eavObjectDAO;
+    private final Class<Directory> directoryClass = Directory.class;
+    public DirectoryService(@Qualifier("EAVObjectDAO")EAVObjectDAO eavObjectDAO) {
+        this.eavObjectDAO = eavObjectDAO;
+    }
+    public Directory findById(BigInteger id) {
+        if (isIdValid(id)) {
+            return  eavObjectDAO.getObjectById(id, directoryClass);
+        }
+        return new Directory();
+    }
+    public void save(Directory directory){
+        if (isDirectoryValid(directory)){
+            eavObjectDAO.saveObject(directory);
+        }
+    }
+
+    public void deleteById(BigInteger id ){
+        if(isIdValid(id)){
+            eavObjectDAO.deleteById(id);
+        }
+    }
+    public List<Directory> getDirectories() {
+        return eavObjectDAO.getObjectsByObjectTypeId(BigInteger.valueOf(4), Directory.class);
+    }
+    private boolean isIdValid(BigInteger id){
+        return id != null && !id.equals(BigInteger.valueOf(0));
+    }
+
+    private boolean isDirectoryValid(Directory directory){
+        if (directory != null){
+            return directory.getPath() != null && directory.getPath().trim().length() != 0 && directory.isActive();
+        }else
+            return false;
+    }
+}
