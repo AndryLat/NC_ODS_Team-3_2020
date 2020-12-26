6ee796a2ec651f81a2dca2caa11e5128c858716a
Valdemar <arnik.20100608@gmail.com>
2020-12-26 14:15:31 +0200

    Add methods (allServer and testConnection)

--- src/main/java/com/netcracker/odstc/logviewer/controller/ServerController.java
+++ src/main/java/com/netcracker/odstc/logviewer/controller/ServerController.java
@@ -1,8 +1,13 @@
 package com.netcracker.odstc.logviewer.controller;
 
 
+import com.netcracker.odstc.logviewer.dao.EAVObjectDAO;
 import com.netcracker.odstc.logviewer.models.Server;
+import com.netcracker.odstc.logviewer.models.User;
+import com.netcracker.odstc.logviewer.serverconnection.services.ServerConnectionService;
 import com.netcracker.odstc.logviewer.service.ServerService;
+import com.netcracker.odstc.logviewer.service.UserService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,17 +20,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigInteger;
+import java.security.Principal;
+import java.util.List;
 
 @RequestMapping("/server")
 @RestController
 public class ServerController {
 
     private final ServerService serverService;
+    private final UserService userService;
+    private final EAVObjectDAO eavObjectDAO;
 
-    public ServerController(ServerService serverService) {
+    public ServerController(ServerService serverService, UserService userService,
+                            @Qualifier("EAVObjectDAO") EAVObjectDAO eavObjectDAO) {
         this.serverService = serverService;
+        this.userService = userService;
+        this.eavObjectDAO = eavObjectDAO;
     }
 
+
+    @GetMapping("/all")
+    public List<Server> allServer(Principal principal){
+        User user = userService.findByName(principal.getName());
+        return eavObjectDAO.getObjectsByParentId(user.getObjectId(), Server.class);
+    }
 
     @PostMapping("/add")
     public ResponseEntity<Server> add(@RequestBody Server server) {
@@ -62,5 +80,9 @@ public class ServerController {
         return new ResponseEntity(HttpStatus.OK);
     }
 
+    @PostMapping("/testConnection")
+    public boolean testConnection(@RequestBody Server server){
+        return ServerConnectionService.getInstance().isServerAvailable(server);
+    }
 }
 
